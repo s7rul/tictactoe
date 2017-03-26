fb3cb474795e3e3766be0e8dac534f62fc09b0c6
Erik s7rul Serrander <erik.serrander@hotmail.com>
2017-03-26 21:49:23 +0200

    moved around things and broke them

--- logic.cpp
+++ logic.cpp
@@ -10,19 +10,6 @@ int charconvert(char input){//converts char to appropriate int
     return 6;
 }
 
-bool checkIfempty(int board[3][3], int x, int y){
-    //check if a plce on the board is empty
-    //return 0 => not empty
-    //return 1 => empty
-    if (board[x][y] == 0){
-        return 1;
-    }
-    else {
-        std::cout << "not empty\n";
-        return 0;
-    }
-}
-
 bool treeout(int board[3][3], bool turn){
     int count = 0;
     int intturn = turn + 1;
@@ -35,62 +22,51 @@ bool treeout(int board[3][3], bool turn){
         }
     }
 
-    if (count >= 3){
+    if (count < 4){
         return 1;
     }
     else return 0;
 }
 
+void extrude(int board[3][3], int turn){
+    int x;
+    int y;
+    char input[2];
+    std::cout << "Extrude: ";
+    std::cin >> input;
+    x = charconvert(input[0]);
+    y = charconvert(input[1]);
+
+    if (board[x][y] == turn){
+        board[x][y] = 0;
+    }
+    //else extrude(board, turn);
+}
+
+void insert(int board[3][3], int turn){
+    int x;
+    int y;
+    char input[2];
+    std::cout << "Insert: ";
+    std::cin >> input;
+    x = charconvert(input[0]);
+    y = charconvert(input[1]);
+
+    if (board[x][y] == 0){
+        board[x][y] = turn;
+    }
+    //else insert(board, turn);
+}
+
 int inputfunk(int board[3][3], bool turn){//handles input and sets board acordingly
-    if (treeout(board, turn) == 0){
-        char input[2];
-        int x;
-        int y;
-        std::cout << "your choice: ";
-        std::cin >> input;
-
-        x = charconvert(input[0]);
-        y = charconvert(input[1]);
-
-        if (x == 6){return 1;}
-        if (y == 6){return 1;}
-
-        if (checkIfempty(board, x, y) == 1){
-            if (turn == 0){
-                board[x][y] = 1;
-            }
-            if (turn == 1){
-                board[x][y] = 2;
-            }
-        }
+    int intturn = turn + 1;
+    if (treeout(board, turn) == 1){
+        insert(board, intturn);
     }
-    else if (treeout(board, turn) == 1){
-        char fromInput[2];
-        char toInput[2];
-        int xfrom;
-        int yfrom;
-        int xto;
-        int yto;
-        int intturn = turn + 1;
-
-        std::cout << "move: ";
-        std::cin >> fromInput;
-        std::cout << "to: ";
-        std::cin >> toInput;
-
-        xfrom = charconvert(fromInput[0]);
-        yfrom = charconvert(fromInput[1]);
-        xto = charconvert(toInput[0]);
-        yto = charconvert(toInput[1]);
-
-        if (board[xfrom][yfrom] == intturn){
-            board[xfrom][yfrom] = 0;
-        }
-        if (board[xto][yto] == 0){
-            board[xto][yto] = intturn;
-        }
+    else if (treeout(board, turn) == 0){
+        extrude(board, intturn);
+        insert(board, intturn);
     }
-
     return 0;
 }
 
